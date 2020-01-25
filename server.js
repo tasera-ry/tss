e09b07f0942f3b7e1ad0d23fe9c01431ce58a9fa
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-01-25 10:45:06 +0200

    add requirements for azure

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const path = require("path");
 
+//azure gives port as an environment variable
+const port = process.env.PORT || 8000;
+
 /**** Express imports ****/
 
 const express = require("express");
 
 const app = express();
+app.get('/', (req, res) => res.send('Best app'))
 
-app.listen(8000, () => {
-    console.log()
+app.listen(port, () => {
+	console.log()
 });
