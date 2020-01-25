cb1c2aa52d87c9fa326af1bb4b8734a5bb7c890a
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-01-26 01:58:32 +0200

    connected server and app, build deployment for azure

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Badge from '@material-ui/core/Badge';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  state = {
+    data: null,
+    pings:0
+  };
+
+  ping = () => {
+    this.setState((state) => {
+      return {pings: state.pings + 1};
+    });
+    this.callApi()
+      .then(res => this.setState({ data: res.ping }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/ping');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    console.log(body);
+    return body;
+  };
+
+  render() {
+	  return (
+		<div className="App">
+		  <header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+			  React frontend.
+			</p>
+      <Badge badgeContent={this.state.pings} color="secondary">
+        <Button variant="contained" onClick={this.ping}>Ping server</Button>
+      </Badge>
       </header>
-    </div>
-  );
+		</div>
+	  );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const path = require("path");
 
-//azure gives port as an environment variable
-const port = process.env.PORT || 8000;
-
 /**** Express imports ****/
-
 const express = require("express");
-
 const app = express();
-app.get('/', (req, res) => res.send('Best app'))
+const port = process.env.PORT || 8000; //azure gives port as an environment variable
+
+app.get('/api/ping', (req, res) => {
+	res.send({ pong: 'Server here' });
+});
+
+app.use('/', express.static('front/build/'))
 
 app.listen(port, () => {
 	console.log()
