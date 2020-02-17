8516142bfaad0426c58d7d8551bf2b48f4ea6ecb
Jukka <heikintalo.jukka@gmail.com>
2020-02-18 00:12:06 +0200

    Add carets, more consistent sizes

--- front/src/Dayview.js
+++ front/src/Dayview.js
@@ -8,6 +8,8 @@ import Box from '@material-ui/core/Box';
 
 
 function Dayview() {
+
+  let date = new Date(Date.now());
   
   return (
     <div class="container">
@@ -25,10 +27,10 @@ function Dayview() {
           justify="space-around"
           alignItems="center"
         >
-            <div>Prev</div>
-            <div>Maanantai</div> 
-            <div>{Date.now()}</div>
-            <div>Next</div>
+            <div class="hoverHand arrow-left"></div>
+            <h1>Maanantai</h1> 
+            <div>{date.toString()}</div>
+            <div class="hoverHand arrow-right"></div>
         </Grid>
         {/* Range info */}
         <Grid
@@ -38,54 +40,54 @@ function Dayview() {
           alignItems="center"
         >
           <Grid item xs={12}>
-            <Box class="info">Päävalvoja paikalla</Box>
+            <h2 class="info greenB">Päävalvoja paikalla</h2>
           </Grid>
         </Grid>
         <Grid
           container
           direction="row"
-          justify="space-around"
-          alignItems="center"
-          style={{background:'cyan'}}
+          justify="flex-start"
+          alignItems="flex-start"
+          style={{background:''}}
         >
-          <Grid item xs={12} sm>
-            <div class="track">
+          <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>Rata 1</p>
               <Box class="clickableBox" >a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+          <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+          <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+          <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+                    <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+                    <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
           </Grid>
-          <Grid item xs={12} sm>
-            <div class="track">
+                    <Grid item xs={12} sm={2}>
+            <div class="track hoverHand">
               <p>1</p>
               <Box class="clickableBox" style={{background:'red'}}>a</Box>
             </div>
@@ -119,7 +121,7 @@ function Dayview() {
           </Grid>
         </div>
       </Grid>
-      <Link style={{color: 'black'}} to='/weekview'>
+      <Link class="arrow-left" style={{color: 'black'}} to='/weekview'>
         <p>Viikkonäkymään</p>
       </Link>
     </div>
