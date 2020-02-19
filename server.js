9a4faaa036b10d25ae6286da87e4ab8d3ae6ca3a
Roni Kallio <roni.kallio@tuni.fi>
2020-02-19 18:16:00 +0200

    Removed knex initialization from project entry point

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const path = require("path");
 
 // Database driver/query builder
-const knex = require("knex")
-
-// Configuration files
-const { connection_config: pg } = require("./config/pg")
+// const knex = require(path.join(__dirname, 'knex', 'knex'))
 
 /**** Express imports ****/
 const express = require("express");
@@ -15,26 +12,14 @@ const router = require("./routes");
 const config = require("./config/config");
 const port = process.env.PORT || config.server.port; //azure gives port as an environment variable
 
-// Database connection initialization
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: process.env.PG_HOST || pg.host,
-        user: process.env.PG_USER || pg.user,
-        // Confirm you've set this environment variable, no password will be
-        // pushed to github, but it can be found in
-        // /root/tietokannan-tiedot.txt on the server
-        password: process.env.PG_PASSWORD || pg.password,
-        database: process.env.PG_DATABASE || pg.database
-    },
-    searchPath: [process.env.PG_SEARCH_PATH || pg.search_path],
-    // Set this to true when writing queries
-    debug: process.env.PG_DEBUG || pg.debug
-})
-
-// db.raw('select 1+1 as result').then((result) => {
-//     result.rows.forEach(console.log)
-// })
+//knex.schema.createTableIfNotExists('member', (table) => {
+//    table.string('name').primary()
+//    table.string('password')
+//}).then(() => {
+//    return knex('member').insert([
+//        {name: 'veeti valvoja', password: 'password'}
+//    ])
+//})
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -48,5 +33,5 @@ app.use("/api", router);
 app.use("/", express.static("front/build/"))
 
 app.listen(port, () => {
-	console.log("Server on "+port)
+    console.log("Server on "+port)
 });
