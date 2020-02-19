8d56abe3098078e53aa2433b9b8bbe8043492d79
Roni Kallio <roni.kallio@tuni.fi>
2020-02-19 23:21:37 +0200

    Fix: delete erroneous from server.js

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const path = require("path");
 
-// Database driver/query builder
-// const knex = require(path.join(__dirname, 'knex', 'knex'))
-
 /**** Express imports ****/
 const express = require("express");
 const app = express();
@@ -11,16 +8,6 @@ const cookieParser = require("cookie-parser")
 const router = require("./routes");
 const config = require("./config/config");
 const port = process.env.PORT || config.server.port; //azure gives port as an environment variable
-
-//knex.schema.createTableIfNotExists('member', (table) => {
-//    table.string('name').primary()
-//    table.string('password')
-//}).then(() => {
-//    return knex('member').insert([
-//        {name: 'veeti valvoja', password: 'password'}
-//    ])
-//})
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
