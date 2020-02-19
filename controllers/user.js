c5e5a03962f078aa02a5ce96aaaaa0c2af1408c8
Roni Kallio <roni.kallio@tuni.fi>
2020-02-19 23:14:13 +0200

    Resolved merge conflict between between develop and database-login, enabled database authentication.
    With this commit we are finally doing database authentication,
    username is queried from member table and password is compared to the
    password hash in that same table. Some mock data is in the database
    unstable, so use the environment variable ENVIRONMENT=unstable to get
    working login. Also required to set variable PG_PASSWORD to get
    database connection, this of course requires you to have a working SSH
    tunnel to the server as well, refer to the email by Kimmo for
    instructions.
    
    Conflicts:
    	routes.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 const { validationResult } = require('express-validator');
 
+const knex = require('../knex/knex')
+
 /*
 *  Login
 *  requires body fields: name, password
@@ -21,51 +23,64 @@ exports.login = async (req, res) => {
   let name = req.body.name;
   let password = req.body.password;
 
-  //TODO compare with db
-  let dbPassword = "$2a$10$qfTq4eSuU7vTIRBVwCzDyeLlYPwLTiB7jLwXmmsXwUC2hR7YQj3a."
-  let dbName = "test"
-  let dbRank = "1"
-  
-  //checks password vs the hash from db
-  bcrypt.compare(password, dbPassword, function(err, result) {
-    if(result === true){
-      
-      /* jsonwebtoken for authenticating api calls
-      *  contains 3 values: succesful login, rank and when the token was made
-      *  sending this token back is required for admin functions
-      *
-      *  JWTs payload is readable by anyone that gets their hands on it. But the third last part
-      *  provides tamper protection so even if the same payload is sent by someone malicious
-      *  nothing happens
-      */
-      /* example token for test test:
-      *  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjp0cnVlLCJyYW5rIjoiMSIsImlhdCI6MTU4MjA4MzAzMX0.ly57wkYEUC5qWF5Bob-55H_7DL8G7lnJNboP7NTCT7o
-      */
-      jwt.sign({ auth: true, rank: dbRank }, config.jwt.secret, function(err, token) {
-        //succesful login
-        if(!err){
-          console.log("LOGIN token: "+token);
-          res.status(200).cookie("access",token).json({
-            login: true,
-            token: token
-          });
-        } else {
-          console.log(err);
-          res.status(401).json({
-            login: false,
-            err: "Login failed"
-          });
-        } 
-      });
+  (knex
+   .from('member')
+   .select('name', 'password')
+   .where({ name: name })
 
-    } else {
-      res.status(401).json({
-        login: false,
-        err: "Login failed"
-      });
-    }
-  });
-};
+   .then((rows) => {
+     const dbUser = rows.pop()
+
+     // TODO compare with db
+     const dbRank = '1'
+
+     //checks password vs the hash from db
+     bcrypt.compare(password, dbUser.password, function(err, result) {
+       if(result === true){
+         
+         /* jsonwebtoken for authenticating api calls
+          *  contains 3 values: succesful login, rank and when the token was made
+          *  sending this token back is required for admin functions
+          *
+          *  JWTs payload is readable by anyone that gets their hands on it. But the third last part
+          *  provides tamper protection so even if the same payload is sent by someone malicious
+          *  nothing happens
+          */
+         /* example token for test test:
+          *  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjp0cnVlLCJyYW5rIjoiMSIsImlhdCI6MTU4MjA4MzAzMX0.ly57wkYEUC5qWF5Bob-55H_7DL8G7lnJNboP7NTCT7o
+          */
+         jwt.sign({ auth: true, rank: dbRank }, config.jwt.secret, function(err, token) {
+           //succesful login
+           if(!err){
+             console.log("LOGIN token: "+token);
+             res.status(200).cookie("access",token).json({
+               login: true,
+               token: token
+             });
+           } else {
+             console.log(err);
+             res.status(401).json({
+               login: false,
+               err: "Login failed"
+             });
+           } 
+         });
+
+       } else {
+         res.status(401).json({
+           login: false,
+           err: "Login failed"
+         });
+       }
+     });
+   }).catch((err) => {
+     console.log(err)
+     res.status(401).json({
+       login: false,
+       err: "Login failed"
+     })
+   }))
+}
 
 /*
 *  Register
