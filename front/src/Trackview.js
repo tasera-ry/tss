a9f876d8741fcad7a058f3777d8a1e565d01ea2b
Miikka Mänttäri <manttari.miikka@gmail.com>
2020-02-26 01:39:04 +0200

    Moved dayToString from Trackview to /utils/Utils.js. Made Dayview display the correct date and added link to trackview.

--- front/src/Trackview.js
+++ front/src/Trackview.js
@@ -7,29 +7,7 @@ import Grid from "@material-ui/core/Grid";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { Container } from "@material-ui/core";
 
-function dayToString(i) {
-  if (i == 1) {
-    return "Maanantai";
-  }
-  if (i == 2) {
-    return "Tiistai";
-  }
-  if (i == 3) {
-    return "Keskiviikko";
-  }
-  if (i == 4) {
-    return "Torstai";
-  }
-  if (i == 5) {
-    return "Perjantai";
-  }
-  if (i == 6) {
-    return "Lauantai";
-  }
-  if (i == 7) {
-    return "Sunnuntai";
-  }
-}
+import { dayToString } from "./utils/Utils";
 
 function Trackview() {
   let date = new Date(Date.now());
---
+++ front/src/utils/Utils.js
@@ -0,0 +1,25 @@
+export function dayToString(i) {
+    if (i == 1) {
+      return "Maanantai";
+    }
+    if (i == 2) {
+      return "Tiistai";
+    }
+    if (i == 3) {
+      return "Keskiviikko";
+    }
+    if (i == 4) {
+      return "Torstai";
+    }
+    if (i == 5) {
+      return "Perjantai";
+    }
+    if (i == 6) {
+      return "Lauantai";
+    }
+    if (i == 7) {
+      return "Sunnuntai";
+    }
+  }
+
+ 
