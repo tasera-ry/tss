f012514e0d03db581461f1d11195d4554558f4bf
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-02-27 13:06:55 +0200

    Saner date check

--- controllers/track.js
+++ controllers/track.js
@@ -2,22 +2,23 @@
 *  Get tracks
 */
 exports.tracks = async (req, res) => {
-  let date = req.params.date;
+  //year-month-day
+  let date = new Date(req.params.date);
+  console.log(date);
   let tracks=[];
-  if(date){
+  
+  //check for valid date object
+  if(date instanceof Date && !isNaN(date.getTime())){
+    
     //TODO get tracks from db with date
     //select track.name, track.availability from tracks where date=?
     
-    //year-month-day
-    console.log(date);
-    var d = Date.parse(date);
-    
-    var trackListObj = {name:"???",date:d,tracks:{"Kivääri":1,"rata2":2,"rata3":1,"rata4":0}};
+    var trackListObj = {name:"???",date:date,tracks:{"Kivääri":1,"rata2":2,"rata3":1,"rata4":0}};
   }
   else{
-    var d = Date.now()
+    date = Date.now()
     
-    var trackListObj = {name:"???",date:d,tracks:{"Kivääri":2,"Pistooli":1}};
+    var trackListObj = {name:"???",date:date,tracks:{"Kivääri":2,"Pistooli":1}};
   }
   
   res.status(200).json(trackListObj);
