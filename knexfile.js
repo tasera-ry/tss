c649f4acdc09ff37c87485031c8f1bf10459a060
Roni Kallio <roni.kallio@tuni.fi>
2020-03-01 20:09:37 +0200

    Drop unused database environments

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,6 @@
 const path = require('path')
 
 module.exports = {
-    unstable: {
-        client: 'pg',
-        connection: {
-            host: '127.0.0.1',
-            user: 'tssuser',
-            password: process.env.DB_PASSWORD,
-            database: 'unstable',
-            charset: 'utf8'
-        },
-
-        debug: true,
-
-        migrations: {
-            directory: path.join(__dirname, 'knex', 'unstable', 'migrations')
-        },
-
-        seeds: {
-            directory: path.join(__dirname, 'knex', 'unstable', 'seeds')
-        }
-    },
-
-    development: {
-        client: 'pg',
-        connection: {
-            host: '127.0.0.1',
-            user: 'tssuser',
-            password: process.env.PG_PASSWORD,
-            database: 'development',
-            charset: 'utf8'
-        },
-
-        debug: false,
-
-        migrations: {
-            directory: path.join(__dirname, 'knex', 'development', 'migrations')
-        },
-        seeds: {
-            directory: path.join(__dirname, 'knex', 'development', 'seeds')
-        }
-    },
-
     // Personal environment is for development purposes. The running database
     // server should have a database matching your unix username, which is used
     // by default. On windows you might need to set the environment variable
