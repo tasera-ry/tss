b43cc02c51e6fda7a53a91ad9c109c4c70298803
Roni Kallio <roni.kallio@tuni.fi>
2020-03-01 19:21:56 +0200

    Add personal and production environments

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
         connection: {
             host: '127.0.0.1',
             user: 'tssuser',
-            password: process.env.PG_PASSWORD,
+            password: process.env.DB_PASSWORD,
             database: 'unstable',
             charset: 'utf8'
         },
@@ -16,6 +16,7 @@ module.exports = {
         migrations: {
             directory: path.join(__dirname, 'knex', 'unstable', 'migrations')
         },
+
         seeds: {
             directory: path.join(__dirname, 'knex', 'unstable', 'seeds')
         }
@@ -38,6 +39,53 @@ module.exports = {
         },
         seeds: {
             directory: path.join(__dirname, 'knex', 'development', 'seeds')
+        }
+    },
+
+    // Personal environment is for development purposes. The running database
+    // server should have a database matching your unix username, which is used
+    // by default. On windows you might need to set the environment variable
+    // USER or USERNAME to the name registered on the server.
+    personal: {
+        client: 'pg',
+        connection: {
+            host: '127.0.0.1',
+            user: 'tssuser',
+            password: process.env.DB_PASSWORD,
+            database: process.env.USER || process.env.USERNAME,
+            charset: 'utf8'
+        },
+
+        debug: process.env.DB_DEBUG || true,
+
+        migrations: {
+            directory: path.join(__dirname, 'knex', 'personal', 'migrations')
+        },
+
+        seeds: {
+            directory: path.join(__dirname, 'knex', 'personal', 'seeds')
+        }
+    },
+
+    // Current production environment.
+    stable: {
+        client: 'pg',
+        connection: {
+            host: '127.0.0.1',
+            user: 'tssuser',
+            password: process.env.DB_PASSWORD,
+            database: 'stable',
+            charset: 'utf8'
+        },
+
+        debug: false,
+        
+        migrations: {
+            directory: path.join(__dirname, 'knex', 'stable', 'migrations')
+        },
+
+        seeds: {
+            directory: path.join(__dirname, 'knex', 'stable', 'seeds')
         }
     }
 }
