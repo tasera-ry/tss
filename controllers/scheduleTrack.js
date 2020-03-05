7f567d297e4c03b7ee10f8aa1084fb09cd952358
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-03-05 15:42:14 +0200

    Value checking

--- controllers/scheduleTrack.js
+++ controllers/scheduleTrack.js
@@ -8,43 +8,58 @@ exports.trackInfoForDay = async (req, res) => {
   //date/:date/track/:id
   console.log("SCHEDULE_TRACK "+req.params.date);
   console.log("SCHEDULE_TRACK "+req.params.id);
+  let date = new Date(req.params.date);
   
-  //works with localhost:3000/api/date/2020-02-20/track/Shooting Track 0
-  (
-    knex
-      .from('range')
-      .select('range.name as rangeName','track.name','track.description','supervisor_id as rangeOfficer','track_supervisor as trackOfficer','track_supervision.notice as trackNotice')
-      .join('range_reservation', 'range.id', '=', 'range_reservation.range_id')
-      .join('track', 'range.id', '=', 'track.range_id')
-      .leftJoin('scheduled_range_supervision', 'range_reservation.id', '=', 'range_reservation_id')
-      .leftJoin('track_supervision', 'scheduled_range_supervision.id', '=', 'scheduled_range_supervision_id')
-      .where('date', req.params.date)
-      //atm track name needs to be exact e.g. 'Shooting Track 0'
-      .where('track.name', req.params.id)
-      //TODO remove hard coded range below
-      .where('range.id', 6)
-      
-      //TODO test below
-      //track supervision has multiple events and the most recent one is the correct you want rest are for history
+  //check for valid date object
+  if(date instanceof Date && !isNaN(date.getTime())){
+    if(req.params.id !== undefined){
+      //get single track
+      //works with localhost:3000/api/date/2020-02-20/track/Shooting Track 0
+      (
+        knex
+          .from('range')
+          .select('range.name as rangeName','track.name','track.description','supervisor_id as rangeOfficer','track_supervisor as trackOfficer','track_supervision.notice as trackNotice')
+          .join('range_reservation', 'range.id', '=', 'range_reservation.range_id')
+          .join('track', 'range.id', '=', 'track.range_id')
+          .leftJoin('scheduled_range_supervision', 'range_reservation.id', '=', 'range_reservation_id')
+          .leftJoin('track_supervision', 'scheduled_range_supervision.id', '=', 'scheduled_range_supervision_id')
+          .where('date', req.params.date)
+          //atm track name needs to be exact e.g. 'Shooting Track 0'
+          .where('track.name', req.params.id)
+          //TODO remove hard coded range below
+          .where('range.id', 6)
+          
+          //TODO test below
+          //track supervision has multiple events and the most recent one is the correct you want rest are for history
 
-      .then((rows) => {
-        console.log(rows);
-        const trackInfo = rows.pop()
-        console.log(trackInfo);
-        
-        //TODO
-        //with id single track, without all
-        
-        let roState = (trackInfo.rangeOfficer !== null) ? true : false;
-        let toState = (trackInfo.trackOfficer !== null) ? true : false;
-        console.log(roState);
-        var trackObj = {trackName:trackInfo.name,description:trackInfo.description,trackOfficer:toState,rangeOfficer:toState,trackNotice:trackInfo.trackNotice};
-      
-        res.status(200).json({
-          track: trackObj
-        });
-      })
-  )
+          .then((rows) => {
+            console.log(rows);
+            const trackInfo = rows.pop()
+            console.log(trackInfo);
+            
+            let roState = (trackInfo.rangeOfficer !== null) ? true : false;
+            let toState = (trackInfo.trackOfficer !== null) ? true : false;
+            console.log(roState);
+            var trackObj = {trackName:trackInfo.name,description:trackInfo.description,trackOfficer:toState,rangeOfficer:toState,trackNotice:trackInfo.trackNotice};
+          
+            res.status(200).json({
+              track: trackObj
+            });
+          })
+      )
+    }
+    else{
+      //TODO all tracks
+      res.status(400).json({
+        err: "get all not implemented"
+      });
+    }
+  }
+  else{
+    res.status(400).json({
+      err: "Invalid date"
+    });
+  }
 };
 
 /*
