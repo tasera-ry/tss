62a8fe1592e7407d604ac01b5b3021233f6e867a
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-03-24 11:03:30 +0200

    Track model

--- models/index.js
+++ models/index.js
@@ -2,5 +2,7 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const models = path.join(root, 'models')
 const user = require(path.join(models, 'user'))
+const track = require(path.join(models, 'track'))
 
 exports.user = user
+exports.track = track
---
+++ models/track.js
@@ -0,0 +1,121 @@
+/* TODO
+ * Replace validations with validator.js
+ */
+const _ = require('lodash')
+const validate = require('validate.js')
+
+const path = require('path')
+const root = path.join(__dirname, '..')
+const knex = require(path.join(root, 'knex', 'knex'))
+//TODO replace with actual range
+const range_id = require(path.join(root, 'config', 'config')).development.range_id
+
+const model = {
+  /** 
+   * Create a new track.
+   *
+   * @param {object} track - Track's properties, { range_id, name, description }
+   * @return {Promise<number[]>} The added tracks id
+   *
+   * @example TODO: range by name?
+   * model.create({ range_id: 1, name: 'Shooting track 1', description: '100m Kohdistusrata'})
+   */
+  create: async function createTrack(trackInfo) {
+    const trackInfo = Object.assign({range_id: range_id}, trackInfo);
+    console.log("MODEL_TRACK_CREATE combined: ",trackInfo);
+    
+    const trackConstraints = {
+      range_id: {}
+      , name: {}
+      , description: {}
+    }
+
+    const track = validate.cleanAttributes(trackInfo, trackConstraints)
+    
+    return await knex.transaction(trx => {
+      return trx
+        .returning('id')
+        .insert(track)
+        .into('track')
+        .then(ids => {
+          return ids
+        }).then(trx.commit)
+        .catch(trx.rollback)
+    })
+  }
+
+  /**
+   * Get the tracks matching a key.
+   *
+   * @param {object} key - Identifying key, { id?, range_id?, name?, description? }
+   * @param {object (TODO: array?)} fields - Attributes about the track to select { id?, range_id?, name?, description? }
+   * @return {Promise<object[]>} Tracks that matched the key
+   *
+   * @example
+   * model.read({ name: 'Shooting track 1' }, ['description'])
+   */
+  , read: async function readTrack(key, fields) {
+    return knex('track')
+      .leftJoin('range', 'range.id', 'track.range_id')
+      .where(key)
+      .select(fields)
+  }
+
+  /**
+   * Update a tracks' info.
+   *
+   * @param {object} current - The current identifying info of the track.
+   * @param {object} update - New information for the track
+   *
+   * @return {Promise<number[]>} Count of rows updated
+   *
+   * @example
+   * exports.update({ name: 'Shooting track 1' }, { description: '200m Kohdistusrata' })
+   */
+  , update: async function updateTrack(current, update) {
+    //TODO: do we allow range_id to be modified for a track?
+    
+    const trackConstraints = {
+      range_id: {}
+      , name: {}
+      , description: {}
+    }
+
+    const track = validate.cleanAttributes(update, trackConstraints)
+
+    const id = await model
+          .read(current, ['id'])
+          .then(rows => rows[0])
+
+    return await knex.transaction(trx => {
+      return trx('track')
+        .where(id)
+        .update(track)
+    })
+  }
+
+  /**
+   * Delete the tracks matching a key.
+   *
+   * @param {object} key - Identifying key, { id?, name? }
+   * @return {Promise<number[]>} Count of deleted tracks
+   *
+   * @example
+   * exports.del({ name: 'Shooting track 1' })
+   */
+  , delete: async function deleteTrack(track) {
+    const ids = await model.read(track, ['id'])
+
+    return await knex.transaction(trx => {
+      return Promise.all(
+        ids.map(id => {
+          return trx('track')
+            .where(id)
+            .del()
+        })).then(trx.commit)
+        .catch(trx.rollback)
+    })
+  }
+}
+
+module.exports = model
