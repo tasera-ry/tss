87af920a6f90c5a1f526d15448e52ac88393bd12
Roni Kallio <roni.kallio@tuni.fi>
2020-03-29 23:41:08 +0300

    Middlewares for reservation API

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const user =  require(path.join(root, 'middlewares', 'user'))
 const jwt = require(path.join(root, 'middlewares', 'jwt'))
+const reservation = require(path.join(root, 'middlewares', 'reservation'))
 
 exports.user = user
 exports.jwt = jwt
+exports.reservation = reservation
