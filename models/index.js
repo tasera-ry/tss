08b4eb444379786977ed2f8bc272b8d5ef2bc32a
Roni Kallio <roni.kallio@tuni.fi>
2020-03-29 18:47:23 +0300

    Database requests for range reservations

--- models/index.js
+++ models/index.js
@@ -2,5 +2,7 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const models = path.join(root, 'models')
 const user = require(path.join(models, 'user'))
+const reservation = require(path.join(models, 'reservation'))
 
 exports.user = user
+exports.reservation = reservation
---
+++ models/reservation.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const root = path.join(__dirname, '..')
+const knex = require(path.join(root, 'knex', 'knex'))
+
+/** 
+ * Create a new reservation.
+ *
+ * @param {object} reservationDetails - The reservations details { date, available }.
+ *
+ * @return {Promise<object[]>} The added reservations details.
+ *
+ * @example
+ * createReservation({ id: 1, date:'2020-01-01', available:true })
+ */
+async function createReservation(reservationDetails) {
+  return knex('range_reservation')
+    .returning('*')
+    .insert(reservationDetails)
+}
+
+/**
+ * Get the reservations matching a key.
+ *
+ * @param {object} key - Identifying key, { id?, range_id?, date?, available? }
+ * @param {object} fields - Attributes about the reservation to select { id?, range_id?, date?, available? }
+ * @return {Promise<object[]>} Reservations that matched the key
+ *
+ * @example
+ * readReservation({ available: true }, [], '2020-01-01', '2020-01-31')
+ */
+async function readReservation(key, fields, from, to) {
+  if(from === undefined) {
+    from = '0001-01-01'
+  }
+
+  if(to === undefined) {
+    to='9999-12-31'
+  }
+
+  return knex('range_reservation')
+    .select(fields)
+    .where((builder =>
+            builder
+            .where(fields)
+            .whereBetween('date', from, to)))
+}
+
+/**
+ * Update reservations' info.
+ *
+ * @param {object} current - The current identifying info of the reservation.
+ * @param {object} update - New information for the reservation
+ *
+ * @return {Promise<object[]>} Updated rows 
+ *
+ * @example
+ * updateReservation({ date: '2020-01-01' }, { available: false })
+ */
+async function updateReservation(current, updates) {
+  const ids = await readReservation(current, ['id']).map(obj => obj.id)
+
+  if(ids.length === 0) {
+    const err = Error('Didn\'t identify reservations to update')
+    err.name = 'Unknown reservation'
+    throw err
+  }
+  
+  return knex('range_reservation')
+    .whereIn('id', ids)
+    .update(updates)
+    .returning('*')
+}
+
+/**
+ * Delete the reservations matching a key.
+ *
+ * @param {object} key - Identifying key
+ * @return {Promise<number>} Count of deleted users
+ *
+ * @example
+ * deleteReservation({ date: '2020-01-01' })
+ */
+async function deleteReservation(key) {
+  return knex('range_reservation')
+    .where(key)
+    .del()
+}
+
+module.exports = {
+  create: createReservation
+  , read: readReservation
+  , update: updateReservation
+  , delete: deleteReservation
+}
