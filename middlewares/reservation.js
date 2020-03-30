cc15e33a66386ff4176f451b624231072e421734
Roni Kallio <roni.kallio@tuni.fi>
2020-03-30 21:55:35 +0300

    Small fixes to reservation api

--- middlewares/reservation.js
+++ middlewares/reservation.js
@@ -12,7 +12,6 @@ async function createReservation(request, response, next) {
   } catch(e) {
     return next(e)
   }
-  console.log(response.locals.queryResult)
   response.set('Location', `api/reservation/${response.locals.queryResult.id}`)
   return next()
 }
