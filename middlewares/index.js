591edb706a95b4eb8357a092e48e33f694268fc3
Roni Kallio <roni.kallio@tuni.fi>
2020-03-30 16:44:00 +0300

    Middlewares for schedule api

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,7 +3,9 @@ const root = path.join(__dirname, '..')
 const user =  require(path.join(root, 'middlewares', 'user'))
 const jwt = require(path.join(root, 'middlewares', 'jwt'))
 const reservation = require(path.join(root, 'middlewares', 'reservation'))
+const schedule = require(path.join(root, 'middlewares', 'schedule'))
 
 exports.user = user
 exports.jwt = jwt
 exports.reservation = reservation
+exports.schedule = schedule
