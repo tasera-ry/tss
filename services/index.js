df381a8ccb591521bd3b5377ee5680bd87e89587
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-04-03 18:47:29 +0300

    track supervision service

--- services/index.js
+++ services/index.js
@@ -2,5 +2,7 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 
 const user = require(path.join(root, 'services', 'user'))
+const trackSupervision = require(path.join(root, 'services', 'trackSupervision'))
 
 exports.user = user
+exports.trackSupervision = trackSupervision
