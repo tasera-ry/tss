9d2ee970ed2c0ce829370018e8a8d7c9b95f6f12
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-04-03 18:03:19 +0300

    track supervision model

--- models/index.js
+++ models/index.js
@@ -2,5 +2,7 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const models = path.join(root, 'models')
 const user = require(path.join(models, 'user'))
+const trackSupervision = require(path.join(models, 'trackSupervision'))
 
 exports.user = user
+exports.trackSupervision = trackSupervision
