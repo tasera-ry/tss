f4f68565e522eb7c757583d743e4527ee49442d9
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-04-03 18:57:13 +0300

    track supervision controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,7 @@ const path = require('path')
 const root = path.join(__dirname, '..')
 const controllers = path.join(root, 'controllers')
 const user = require(path.join(controllers, 'user'))
+const trackSupervision = require(path.join(controllers, 'trackSupervision'))
 
 exports.user = user
+exports.trackSupervision = trackSupervision
