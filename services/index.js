c2edf341327e81423ae282b703956b5599e4d2cc
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-04-11 20:11:43 +0300

    add service

--- services/index.js
+++ services/index.js
@@ -6,9 +6,11 @@ const track = require(path.join(root, 'services', 'track'))
 const reservation = require(path.join(root, 'services', 'reservation'))
 const schedule = require(path.join(root, 'services', 'schedule'))
 const trackSupervision = require(path.join(root, 'services', 'trackSupervision'))
+const rangeSupervision = require(path.join(root, 'services', 'rangeSupervision'))
 
 exports.user = user
 exports.trackSupervision = trackSupervision
+exports.rangeSupervision = rangeSupervision
 exports.reservation = reservation
 exports.schedule = schedule
 exports.track = track
