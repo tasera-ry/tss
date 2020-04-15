816d9eff864b200187e4a8bf58c8d30e12e0cb50
Roni Kallio <roni.kallio@tuni.fi>
2020-04-15 15:38:32 +0300

    Hotfix hostiin

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 
 const config = {
   server: {
-    host: 'http://tss.netum.fi',
+    host: process.env.SERVER_HOST || 'http://localhost:3000',
     port: 8000
   },
   jwt: {
