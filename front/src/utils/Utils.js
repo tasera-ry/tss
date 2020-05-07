067cf9bac1207e7790bc4c5f474d74cb30e95016
Jukka Heikintalo <heikintalo.jukka@gmail.com>
2020-05-07 15:43:01 +0300

    Util dates follow language

--- front/src/utils/Utils.js
+++ front/src/utils/Utils.js
@@ -45,64 +45,15 @@ export async function getSchedulingWeek(date) {
 }
 
 export function dayToString(i) {
-  if (i === 1) {
-    return "Maanantai";
-  }
-  if (i === 2) {
-    return "Tiistai";
-  }
-  if (i === 3) {
-    return "Keskiviikko";
-  }
-  if (i === 4) {
-    return "Torstai";
-  }
-  if (i === 5) {
-    return "Perjantai";
-  }
-  if (i === 6) {
-    return "Lauantai";
-  }
-  if (i === 0) {
-    return "Sunnuntai";
-  }
+  const lang = localStorage.getItem("language") === '0' ? 'fi' : 'en';
+  moment.locale(lang);
+  //en/fi have different numbers for start date
+  if(lang === 'fi') i--;
+  return moment().weekday(i).format('dddd').toUpperCase();
 }
 
 export function monthToString(i) {
-  if (i === 0) {
-    return "Tammikuu";
-  }
-  if (i === 1) {
-    return "Helmikuu";
-  }
-  if (i === 2) {
-    return "Maaliskuu";
-  }
-  if (i === 3) {
-    return "Huhtikuu";
-  }
-  if (i === 4) {
-    return "Toukokuu";
-  }
-  if (i === 5) {
-    return "Kesäkuu";
-  }
-  if (i === 6) {
-    return "Heinäkuu";
-  }
-  if (i === 7) {
-    return "Elokuu";
-  }
-  if (i === 8) {
-    return "Syyskuu";
-  }
-  if (i === 9) {
-    return "Lokakuu";
-  }
-  if (i === 10) {
-    return "Marraskuu";
-  }
-  if (i === 11) {
-    return "Joulukuu";
-  }
+  const lang = localStorage.getItem("language") === '0' ? 'fi' : 'en';
+  moment.locale(lang);
+  return moment().month(i).format('MMMM');
 }
