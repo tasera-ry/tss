ff432dcd5ff9766c2a5524f58cec443c87d33db5
Jesse Liljenmaa <jessenposti@yahoo.co.uk>
2020-10-20 14:31:40 +0300

    Better styled routes in routes.js

--- routes.js
+++ routes.js
@@ -13,157 +13,161 @@ const oldSchedule = require(path.join(root, 'controllers', 'oldSchedule'));
 
 router.route('/sign')
   .post(
-    middlewares.user.sign
-    , controllers.user.sign)
+    middlewares.user.sign,
+    controllers.user.sign)
 
 router.route('/user')
   .all(
     middlewares.jwt.read)
   .get(
-    middlewares.user.readFilter
-    , controllers.user.readFilter)
+    middlewares.user.readFilter,
+    controllers.user.readFilter)
   .post(
-    middlewares.user.hasProperty('role', 'superuser')
-    , middlewares.user.create
-    , controllers.user.create)
+    middlewares.user.hasProperty('role', 'superuser'),
+    middlewares.user.create,
+    controllers.user.create)
 
 router.route('/user/:id')
   .all(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', 'superuser'))
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'))
   .get(
-    middlewares.user.read
-    , controllers.user.read)
+    middlewares.user.read,
+    controllers.user.read)
   .put(
-    middlewares.user.update
-    , controllers.user.update)
+    middlewares.user.update,
+    controllers.user.update)
   .delete(
-    middlewares.user.delete
-    , controllers.user.delete)
+    middlewares.user.delete,
+    controllers.user.delete)
 
 
-//Track supervision
+// Track supervision
 router.route('/track-supervision')
   .get(
-    middlewares.trackSupervision.readFilter
-    , controllers.trackSupervision.readFilter)
+    middlewares.trackSupervision.readFilter,
+    controllers.trackSupervision.readFilter)
   .post(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes)
-    , middlewares.trackSupervision.create
-    , controllers.trackSupervision.create)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes),
+    middlewares.trackSupervision.create,
+    controllers.trackSupervision.create)
 
 router.route('/track-supervision/:scheduled_range_supervision_id/:track_id')
   .get(
-    middlewares.trackSupervision.read
-    , controllers.trackSupervision.read)
+    middlewares.trackSupervision.read,
+    controllers.trackSupervision.read)
   .put(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes)
-    , middlewares.trackSupervision.update
-    , controllers.trackSupervision.update)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes),
+    middlewares.trackSupervision.update,
+    controllers.trackSupervision.update)
   .delete(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', 'superuser')
-    , middlewares.trackSupervision.delete
-    , controllers.trackSupervision.delete)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    middlewares.trackSupervision.delete,
+    controllers.trackSupervision.delete)
 
-//Range supervision
+// Range supervision
 router.route('/range-supervision')
   .get(
-    middlewares.rangeSupervision.readFilter
-    , controllers.rangeSupervision.readFilter)
+    middlewares.rangeSupervision.readFilter,
+    controllers.rangeSupervision.readFilter)
   .post(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes)
-    , middlewares.rangeSupervision.create
-    , controllers.rangeSupervision.create)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes),
+    middlewares.rangeSupervision.create,
+    controllers.rangeSupervision.create)
 
 router.route('/range-supervision/:scheduled_range_supervision_id')
   .get(
-    middlewares.rangeSupervision.read
-    , controllers.rangeSupervision.read)
+    middlewares.rangeSupervision.read,
+    controllers.rangeSupervision.read)
   .put(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes)
-    , middlewares.rangeSupervision.update
-    , controllers.rangeSupervision.update)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes),
+    middlewares.rangeSupervision.update,
+    controllers.rangeSupervision.update)
   .delete(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', 'superuser')
-    , middlewares.rangeSupervision.delete
-    , controllers.rangeSupervision.delete)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    middlewares.rangeSupervision.delete,
+    controllers.rangeSupervision.delete)
 
 
 router.route('/reservation')
   .get(controllers.reservation.read)
-  .post(middlewares.jwt.read
-        , middlewares.user.hasProperty('role', 'superuser')
-        , controllers.reservation.create)
+  .post(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    controllers.reservation.create)
 
 router.route('/reservation/:id')
   .get(controllers.reservation.readStrict)
-  .put(middlewares.jwt.read
-       , middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes)
-       , controllers.reservation.update)
-  .delete(middlewares.jwt.read
-          , middlewares.user.hasProperty('role', 'superuser')
-          , controllers.reservation.delete)
+  .put(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser','supervisor'], _.includes),
+    controllers.reservation.update)
+  .delete(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    controllers.reservation.delete)
 
 router.route('/schedule')
   .get(controllers.schedule.read)
-  .post(middlewares.jwt.read
-        , middlewares.user.hasProperty('role', 'superuser')
-        , controllers.schedule.create)
+  .post(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    controllers.schedule.create)
 
 router.route('/schedule/:id')
   .get(controllers.schedule.readStrict)
-  .put(middlewares.jwt.read
-       , middlewares.user.hasProperty('role', ['superuser', 'supervisor'], _.includes)
-       , controllers.schedule.update)
-  .delete(middlewares.jwt.read
-          , middlewares.user.hasProperty('role', 'superuser')
-          , controllers.schedule.delete)
+  .put(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', ['superuser', 'supervisor'], _.includes),
+    controllers.schedule.update)
+  .delete(
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    controllers.schedule.delete)
 
 /*
 *  Track
 */
 router.route('/track')
   .get(
-    validators.track.readAll
-    , middlewares.track.read
-    , controllers.track.read)
+    validators.track.readAll,
+    middlewares.track.read,
+    controllers.track.read)
   .post(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', 'superuser')
-    , validators.track.create
-    , middlewares.track.create
-    , controllers.track.create)
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'),
+    validators.track.create,
+    middlewares.track.create,
+    controllers.track.create)
 
 router.route('/track/:track_id')
   .all(
-    middlewares.jwt.read
-    , middlewares.user.hasProperty('role', 'superuser'))
+    middlewares.jwt.read,
+    middlewares.user.hasProperty('role', 'superuser'))
   .get(
-    validators.track.read
-    , middlewares.track.read
-    , controllers.track.read)
+    validators.track.read,
+    middlewares.track.read,
+    controllers.track.read)
   .put(
-    validators.track.update
-    , middlewares.track.update
-    , controllers.track.update)
+    validators.track.update,
+    middlewares.track.update,
+    controllers.track.update)
   .delete(
-    validators.track.delete
-    , middlewares.track.delete
-    , controllers.track.delete)
+    validators.track.delete,
+    middlewares.track.delete,
+    controllers.track.delete)
 
-//newer get with padded functionality
+// newer get with padded functionality
 router.route("/datesupreme/:date")
-  .get(
-    oldSchedule.getScheduleDate)
+  .get(oldSchedule.getScheduleDate)
 
 router.route("/validate")
-  .get(
-    middlewares.jwt.validate)
+  .get(middlewares.jwt.validate)
 
 module.exports = router;
