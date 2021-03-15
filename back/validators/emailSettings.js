4a8fa81842e0ccc7aa79c41e08f2043775d4ca54
aleves64 <aleksi.vesterinen@gmail.com>
2021-03-15 15:03:35 +0200

    Add validation for email message contents

--- back/validators/emailSettings.js
+++ back/validators/emailSettings.js
@@ -1,5 +1,20 @@
 const { checkSchema, validationResult, matchedData } = require('express-validator');
 
+/* Returns a custom validation function for the email messages */
+const checkEmailMessage = (allowedVars) => {
+  return (value, _) => {
+    return new Promise((resolve, reject) => {
+      const matches = value.match(/{(.*?)}/g) || [];
+      matches.forEach(match => {
+        console.log(match);
+        if (!allowedVars.includes(match)) {
+          return reject();
+        }
+      });
+      return resolve();
+    });
+  };
+};
 
 /*
  This object contains the constraints that the received data is checked against.
@@ -13,11 +28,11 @@ const emailSettingConstraints = {
   port: {in: ["body"], exists: true, isInt: true, errorMessage: "Invalid port"},
   secure: {in: ["body"], exists: true},
   shouldSend: {in: ["body"], exists: true},
-  assignedMsg: {in: ["body"]},
-  updateMsg: {in: ["body"]},
-  reminderMsg: {in: ["body"]},
-  declineMsg: {in: ["body"]},
-  feedbackMsg: {in: ["body"]}
+  assignedMsg: {in: ["body"], custom: {options: checkEmailMessage([])}},
+  updateMsg: {in: ["body"], custom: {options: checkEmailMessage([])}},
+  reminderMsg: {in: ["body"], custom: {options: checkEmailMessage([])}},
+  declineMsg: {in: ["body"], custom: {options: checkEmailMessage(["{date}", "{user}"])}},
+  feedbackMsg: {in: ["body"], custom: {options: checkEmailMessage(["{feedback}", "{user}"])}}
 };
 
 function handleValidationErrors(request, response, next) {
