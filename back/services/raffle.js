6145450ca7feb25dfcbc19edd37ff105666929b4
Henri Laakso <lake.laakso@gmail.com>
2021-10-22 19:41:48 +0300

    weekday from date not working yet

--- back/services/raffle.js
+++ back/services/raffle.js
@@ -12,8 +12,11 @@ const service = {
    * @returns 
    */
   create: async function createRaffle(info) {
-    const dates = await models.reservation.read({ available: true }, [], '2021-07-01', '2021-12-31')
-    return dates;
+    const dates = await models.reservation.read({ available: true }, [], '2021-01-01', '2021-12-31');
+    const len = dates.length;
+    const str = dates[0].date
+    const day = new Date('str');
+    return {"available days in 2021": len};
   }
 };
 
