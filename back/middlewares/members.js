a30a9f1a6443b4572705eee68a3db7648eed953c
Henri Laakso <lake.laakso@gmail.com>
2021-10-26 14:43:11 +0300

    members get doned

--- back/middlewares/members.js
+++ back/middlewares/members.js
@@ -5,22 +5,18 @@ const services = require(path.join(root, 'services'));
 const _ = require('lodash');
 
 const serviceCalls = {
-  read: async function readUser(request, response, next) {
+  read: async function readMembers(request, response, next) {
     const query = response.locals.query;
 
     try {
-      response.locals.queryResult = await services.user.read(query, []);
+      response.locals.queryResult = await services.members.read(query, []);
     }
     catch(e) {
       return next(e);
     }
 
     return next();
-  },
+  }
 };
 
-exports.read = [
-  //TODO add validator
-  //validators.members.read,
-  serviceCalls.read
-];
+module.exports = serviceCalls;
