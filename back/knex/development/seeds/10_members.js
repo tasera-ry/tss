6848d342567be844c78f573ae160aa1a30f0de89
Iida Kainu <iida.kainu@gmail.com>
2021-10-26 10:15:39 +0300

    Add members seed data

---
+++ back/knex/development/seeds/10_members.js
@@ -0,0 +1,17 @@
+exports.seed = async function (knex) {
+  // get all supervisor users
+  const users = await knex('user').where({ role: 'supervisor' }).select('id');
+  // generate random numbers for ranging from 1-100 for members & 1-10 for supervisors
+  const members = users.map(({ id }) => ({
+    user_id: id,
+    members: Math.floor(Math.random() * 100) + 1,
+    supervisors: Math.floor(Math.random() * 10) + 1,
+  }));
+
+  return knex('members')
+    .del()
+    .then(function () {
+      // insert seed entries
+      return knex('members').insert(members);
+    });
+};
