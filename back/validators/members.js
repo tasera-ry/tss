136b408ef7fffe80bc0e32bb93c6b542db77b4b2
Henri Laakso <lake.laakso@gmail.com>
2021-10-26 16:09:16 +0300

    both members and members/id get working

--- back/validators/members.js
+++ back/validators/members.js
@@ -1,1 +1,64 @@
-//TODO this
+const _ = require('lodash');
+
+const {
+  body,
+  query,
+  param,
+  validationResult,
+  matchedData
+} = require('express-validator');
+
+function validatorAdditions(validator, opts) {
+  if(opts.includes('exists')) {
+    validator = validator
+      .exists({ checkNull: true, checkFalsy: true })
+      .withMessage('must be included');
+  }
+
+  if(opts.includes('optional')) {
+    validator = validator
+      .optional();
+  }
+
+  return validator;
+}
+
+const fields = {
+  user_id: function idValidation(requestObject, ...opts) {
+    const validator = requestObject('user_id')
+      .isInt()
+      .withMessage('must be an integer')
+      .toInt();
+
+    return validatorAdditions(validator, opts);
+  }
+};
+
+function handleValidationErrors(request, response, next) {
+  const validationErrors = validationResult(request);
+
+  if(validationErrors.isEmpty() === false) {
+    return response.status(400).send(validationErrors);
+  }
+
+  return next();
+}
+
+module.exports = {
+  read: [
+    fields.user_id(param, 'exists'),
+    handleValidationErrors,
+    function storeID(request, response, next) {
+      response.locals.query = matchedData(request, { locations: ['params'] });
+      return next();
+    }
+  ],
+  readAll: [
+    handleValidationErrors,
+    function storeID(request, response, next) {
+      response.locals.filtered = !_.isEmpty(request.query);
+      response.locals.query = matchedData(request, { locations: ['params', 'query'] });
+      return next();
+    }
+  ]
+}
