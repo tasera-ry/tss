26ab5b46e52646d3ca720be452af55bbfdbdafdd
jozura <johannes.haapakoski@outlook.com>
2021-11-21 13:51:04 +0200

    Refactor user mock from all test modules into own single module.

---
+++ back/models/__mocks__/user.js
@@ -0,0 +1,28 @@
+/* eslint-env jest */
+const users = [
+  
+]; 
+// Mock user model
+const model = {
+  read: async (params) => users.filter((user) => {
+    const searchKeys = Object.keys(params);
+    let match = true;
+    searchKeys.forEach(key => {
+      if (params[key] !== user[key]) {
+        match = false;
+      }
+    });
+    return match;
+  }),
+  create: async (user) => { 
+    users.push(user);
+    return [user];
+  },
+  update: async (current, update) => {
+    const i = users.findIndex(user => user.id == current.id);
+    users[i] = {...users[i], ...update};
+  },
+  clear: () => users.length = 0
+};
+
+module.exports = model;
