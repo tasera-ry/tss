00f42babaf1111076f662e126b15735f951a5cae
Patric Kangasmaki <patric.kangasmaki@hotmail.com>
2021-12-09 10:41:00 +0200

    Improve styling of SignIn -page

--- front/src/signin/SignIn.js
+++ front/src/signin/SignIn.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
-import colors from '../colors.module.scss';
 
 // Material UI components
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import { useHistory } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import colors from '../colors.module.scss';
 import api from '../api/api';
 import translations from '../texts/texts.json';
 import css from './SignIn.module.scss';
 
 const classes = classNames.bind(css);
+
+/*
+  TO DO: This same theme is used in ResetPassword.js
+  -Find out if this theme can be applied to both without two separate declarations
+  (without causing side-effects to other parts of the app)
+*/
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.grey,
+    },
+  },
+});
 
 /* Returns a component for signing in to the frontend */
 const SignIn = () => {
@@ -49,82 +67,80 @@ const SignIn = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes(css.paper)}>
-        <Typography component="h1" variant="h5">
-          {signin.SignIn[lang]}
-        </Typography>
-
-        <form noValidate className={classes(css.wideForm)}>
-          <TextField
-            autoFocus
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            name="username"
-            label={signin.Name[lang]}
-            autoComplete={signin.Name[lang]}
-            value={name}
-            error={mistake}
-            onInput={(e) => setName(e.target.value)}
-            className={classes(css.text)}
-            inputProps={{
-              'data-testid': 'nameField',
-            }}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="password"
-            name="password"
-            label={signin.Password[lang]}
-            type="password"
-            autoComplete="current-password"
-            value={password}
-            error={mistake}
-            onInput={(e) => setPassword(e.target.value)}
-            className={classes(css.text)}
-            inputProps={{
-              'data-testid': 'passwordField',
-            }}
-          />
-          {mistake && (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes(css.paper)}>
+          <div className={classes(css.flexWrap)}>
+            <ArrowBackIcon
+              className={classes(css.arrowBackIcon)}
+              onClick={() => history.push('/')}
+            />
+            <Typography component="h1" variant="h5">
+              {signin.SignIn[lang]}
+            </Typography>
+          </div>
+          <form noValidate className={classes(css.wideForm)}>
+            <TextField
+              autoFocus
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              name="username"
+              label={signin.Name[lang]}
+              autoComplete={signin.Name[lang]}
+              value={name}
+              error={mistake}
+              onInput={(e) => setName(e.target.value)}
+              className={classes(css.text)}
+              inputProps={{
+                'data-testid': 'nameField',
+              }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label={signin.Password[lang]}
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              error={mistake}
+              onInput={(e) => setPassword(e.target.value)}
+              className={classes(css.text)}
+              inputProps={{
+                'data-testid': 'passwordField',
+              }}
+            />
+            {mistake && (
             <Typography align="center" className={classes(css.error)}>
               {signin.Helper[lang]}
             </Typography>
-          )}
-          <Button
-            onClick={login}
-            fullWidth
-            variant="contained"
-            className={classes(css.submitButton, css.acceptButton)}
-          >
-            {signin.LogIn[lang]}
-          </Button>
-          &nbsp;
-          <Button
-            onClick={() => history.goBack()}
-            fullWidth
-            className={classes(css.secondaryButton)}
-          >
-            {signin.Back[lang]}
-          </Button>
-          &nbsp;
-          <Button
-            onClick={() => history.push('/signin/reset-password')}
-            fullWidth
-            className={classes(css.secondaryButton)}
-          >
-            {signin.ForgotPassword[lang]}
-          </Button>
-        </form>
-      </div>
-    </Container>
+            )}
+            <Button
+              onClick={login}
+              fullWidth
+              variant="contained"
+              className={classes(css.submitButton, css.acceptButton)}
+            >
+              {signin.LogIn[lang]}
+            </Button>
+            <Button
+              onClick={() => history.push('/signin/reset-password')}
+              fullWidth
+              className={classes(css.secondaryButton)}
+            >
+              {signin.ForgotPassword[lang]}
+            </Button>
+          </form>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 };
 
