7e01cd958cafa32943f2cc496265ff819e9d500d
lauri-ra <lrautiala+1@gmail.com>
2024-03-05 17:30:15 +0200

    Updated seed files to match role changes in db

--- back/knex/development/seeds/02_users.js
+++ back/knex/development/seeds/02_users.js
@@ -8,51 +8,51 @@ const casual = require('casual');
 const bcrypt = require('bcryptjs');
 const ora = require('ora');
 
+function getRandomRole() {
+	const roles = ['superuser', 'association', 'rangeofficer'];
+	const randomIndex = Math.floor(Math.random() * roles.length);
+	return roles[randomIndex];
+}
+
 casual.seed(config.seeds.seed);
-exports.seed = async function(knex) {
-  const listedUsersAmount = Math.min(config.seeds.users, 50);
+exports.seed = async function (knex) {
+	const listedUsersAmount = Math.min(config.seeds.users, 50);
 
-  // do we really need terminal spinners?
-  const generateUsers = Promise.all(_.times(config.seeds.users, casual._user));
-  ora.promise(
-    generateUsers,
-    `Generating ${config.seeds.users} users`);
-  const users = await generateUsers;
+	// do we really need terminal spinners?
+	const generateUsers = Promise.all(_.times(config.seeds.users, casual._user));
+	ora.promise(generateUsers, `Generating ${config.seeds.users} users`);
+	const users = await generateUsers;
 
-  const head = _.take(users, listedUsersAmount)
-    .map(_.partialRight(_.pick, ['name', 'role', 'password', 'email']));
-  // simplify JSON.stringify
-  console.log(`First ${listedUsersAmount} users:\n${JSON.stringify(head, null, 2)}`);
+	const head = _.take(users, listedUsersAmount).map(
+		_.partialRight(_.pick, ['name', 'role', 'password', 'email'])
+	);
+	// simplify JSON.stringify
+	console.log(`First ${listedUsersAmount} users:\n${JSON.stringify(head, null, 2)}`);
 
-  const insertUsers = Promise.all(
-    _.chunk(users, config.seeds.chunkSize)
-      .map(async (userChunk) => {
-        const users = userChunk
-          .map(user => _.pick(user, ['name', 'role', 'digest','email']));
-        const supervisors =
-              (await knex('user')
-                .insert(users)
-                .returning(['id as user_id', 'role']))
-                .map((user, i) => _.extend(user, _.pick(userChunk[i], ['user_id', 'phone'])))
-                .filter(user => user.role === 'supervisor')
-                .map(user => _.pick(user, ['user_id', 'phone']));
+	const insertUsers = Promise.all(
+		_.chunk(users, config.seeds.chunkSize).map(async (userChunk) => {
+			const users = userChunk.map((user) => _.pick(user, ['name', 'role', 'digest', 'email']));
+			const associations = (await knex('user').insert(users).returning(['id as user_id', 'role']))
+				.map((user, i) => _.extend(user, _.pick(userChunk[i], ['user_id', 'phone'])))
+				.filter((user) => user.role === 'association')
+				.map((user) => _.pick(user, ['user_id', 'phone']));
 
-        await knex('supervisor')
-          .insert(supervisors);
-      }));
-  ora.promise(insertUsers, 'Inserting users');
-  await insertUsers;
+			await knex('association').insert(associations);
+		})
+	);
+	ora.promise(insertUsers, 'Inserting users');
+	await insertUsers;
 };
 
-casual.define('user', async function() {
-  const password = casual.password;
-  return {
-    name: casual.username,
-    password: password,
-    digest: bcrypt.hashSync(password, 0),
-    phone: casual.phone,
-    email: casual.email,
-    
-    role: casual.integer(0, 4) ? 'supervisor' : 'superuser'
-  };
+casual.define('user', async function () {
+	const password = casual.password;
+	return {
+		name: casual.username,
+		password: password,
+		digest: bcrypt.hashSync(password, 0),
+		phone: casual.phone,
+		email: casual.email,
+
+		role: getRandomRole(),
+	};
 });
