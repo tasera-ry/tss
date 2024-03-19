368c227f36b1ee3ef8addaff750018649ff95189
lauri-ra <lrautiala+1@gmail.com>
2024-03-19 20:27:55 +0200

    Added notifications & better error handling to rangeofficer form

--- front/src/profile/profilepages/rangeofficers.js
+++ front/src/profile/profilepages/rangeofficers.js
@@ -1,26 +1,57 @@
 import React, { useState } from 'react';
-import classNames from 'classnames';
-import api from '../../api/api';
+
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import { Alert } from '@material-ui/lab';
+
+import classNames from 'classnames';
 import translations from '../../texts/texts.json';
 import css from './ChangePassword.module.scss';
+import api from '../../api/api';
 
 const classes = classNames.bind(css);
 
 const lang = localStorage.getItem('language');
 const { rangeofficerSettings } = translations;
 
+/**
+ * Component for creating new range officers
+ * @param {number} id - Logged in association id from cookies
+ */
+
 export default function RangeOfficers({ id }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [notification, setNotification] = useState(null);
 
-  // TODO: add api call where range officer user is created
-  // add the range officer to the association table
+  // Function for creating notifications. Clears after 3 seconds
+  const createNotification = (type, message) => {
+    setNotification({ type, message });
+
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
+
+  // Function for creating an user when form is submitted
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    // check if all fields are filled
+    if (!username || !password || !passwordConfirm) {
+      createNotification('error', rangeofficerSettings.empty[lang]);
+      return;
+    }
+
+    // Check if passwords match
+    if (password !== passwordConfirm) {
+      createNotification('error', rangeofficerSettings.passwordMatch[lang]);
+      return;
+    }
+
+    // Create new user object with rangeoffice role
     const newUser = {
       name: event.target.username.value,
       role: 'rangeofficer',
@@ -29,9 +60,18 @@ export default function RangeOfficers({ id }) {
     };
 
     try {
+      // Send new user to backend
       await api.createUser(newUser);
+
+      // Send notification & clear input fields
+      createNotification('success', rangeofficerSettings.sucess[lang]);
+      setUsername('');
+      setPassword('');
+      setPasswordConfirm('');
     } catch (err) {
+      // log error and send notification
       console.log(err);
+      createNotification('error', rangeofficerSettings.error[lang]);
     }
   };
 
@@ -72,9 +112,31 @@ export default function RangeOfficers({ id }) {
           onInput={(e) => setPassword(e.target.value)}
           className={classes(css.textField)}
         />
-        <Button type="submit" fullWidth variant="contained">
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="passwordConfirm"
+          name="passwordConfrim"
+          value={passwordConfirm}
+          type="password"
+          label={rangeofficerSettings.passwordConfirm[lang]}
+          onInput={(e) => setPasswordConfirm(e.target.value)}
+          className={classes(css.textField)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          className={classes(css.acceptButton)}
+        >
           {rangeofficerSettings.confirm[lang]}
         </Button>
+
+        {notification && (
+          <Alert severity={notification.type}>{notification.message}</Alert>
+        )}
       </form>
     </div>
   );
