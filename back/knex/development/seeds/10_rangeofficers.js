31dcba573024c1c75a96543c2edaaa1f695d84c1
lauri-ra <lrautiala+1@gmail.com>
2024-03-21 23:04:48 +0200

    update rangeofficer seed file

--- back/knex/development/seeds/10_rangeofficers.js
+++ back/knex/development/seeds/10_rangeofficers.js
@@ -2,27 +2,39 @@ const _ = require('lodash');
 const ora = require('ora');
 
 exports.seed = async function (knex) {
-    const associations = await knex.select('id').from('user').where('role', 'association');
-    const rangeofficers = await knex.select('id').from('user').where('role', 'rangeofficer');
+  // Get all associations and range officers
+  const associations = await knex
+    .select('id')
+    .from('user')
+    .where('role', 'association');
 
-    const associationRangeOfficers = [];
-    associations.forEach((association) => {
-      // Randomly select range officers for each association
-      const selectedRangeOfficers = _.sampleSize(rangeofficers, _.random(1, 3)); // Adjust the range as needed
+  const rangeofficers = await knex
+    .select('id')
+    .from('user')
+    .where('role', 'rangeofficer');
 
-      selectedRangeOfficers.forEach((rangeofficer) => {
-        associationRangeOfficers.push({
-          association_id: association.id,
-          rangeofficer_id: rangeofficer.id
+  const associationRangeOfficers = [];
+
+  // Add range officers to each association
+  associations.forEach((association) => {
+    // Randomly select range officers for each association
+    const selectedRangeOfficers = _.sampleSize(rangeofficers, _.random(2, 4));
+
+    selectedRangeOfficers.forEach((rangeofficer) => {
+      associationRangeOfficers.push({
+        association_id: association.id,
+        rangeofficer_id: rangeofficer.id,
       });
     });
   });
 
-    // Insert data into the association_rangeofficers table
-    const insertAssociationRangeOfficers = await knex('association_rangeofficers').insert(associationRangeOfficers);
+  // Insert data into the association_rangeofficers table
+  const insertAssociationRangeOfficers = await knex(
+    'association_rangeofficers'
+  ).insert(associationRangeOfficers);
 
-    ora.promise(
-      Promise.resolve(insertAssociationRangeOfficers),
-       'Inserting association range officers'
-    );
+  ora.promise(
+    Promise.resolve(insertAssociationRangeOfficers),
+    'Inserting association range officers'
+  );
 };
