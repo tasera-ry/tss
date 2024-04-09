48e51708c99f95498b7c481301a711ddfd3fb444
Markus Koivu <markus.koivu@tuni.fi>
2024-04-09 11:41:43 +0300

    Added functionality for ButtonComponent for fetching device data and status

--- front/src/ButtonComponent/ButtonComponent.js
+++ front/src/ButtonComponent/ButtonComponent.js
@@ -1,16 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styles from './ButtonComponent.module.scss';
 import Button from '@material-ui/core/Button';
+import api from '../api/api';
 
 class ButtonComponent extends Component {
+    state = {
+        devices: [],
+    };
+
+    componentDidMount() {
+        this.fetchDevices();
+    }
+
+    fetchDevices = async () => {
+        try {
+            const response = await api.getAllDevices();
+            this.setState({ devices: response });
+        } catch (error) {
+            console.error('Error fetching devices:', error);
+        }
+    };
+
+    handleStatusChange = async (device) => {
+        // Copy the current devices state
+        const devicesUpdated = this.state.devices.map(item => {
+            if (item.id === device.id) {
+                return { ...item, status: item.status === 'free' ? 'reserved' : 'free' };
+            }
+            return item;
+        });
+
+        try {
+            // Assuming patchDevice returns the updated device, else just use devicesUpdated directly
+            await api.patchDevice(device.id, { status: device.status === 'free' ? 'reserved' : 'free' });
+            this.setState({ devices: devicesUpdated });
+        } catch (error) {
+            console.error('Error updating device status:', error);
+        }
+    };
+
     render() {
+        const { devices } = this.state;
+
+        if (devices.length > 0) {
+            return (
+                <div className={styles.singularGrid}>
+                    {devices.map((device) => (
+                        <div key={device.id}>
+                            <Button className={styles.customButton}>
+                                {device.device_name}
+                            </Button>
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+
+        // Render when there are no devices or if devices are still being fetched
         return (
             <div className={styles.singularGrid}>
-                <Button className={styles.customButton}>Timer 1</Button>
-                <Button className={styles.customButton}>Timer 2</Button>
-                <Button className={styles.customButton}>Labradar</Button>
+                <Button className={styles.customButton}>Fetching Devices...</Button>
             </div>
         );
     }
 }
+
 export default ButtonComponent;
