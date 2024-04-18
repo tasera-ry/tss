f205cd753a0dd981b7d1d95b24bbf9c1b889a9f9
Markus Koivu <markus.koivu@tuni.fi>
2024-04-18 14:35:10 +0200

    Added translations to Buttoncomponent and translations to texts.json

--- front/src/ButtonComponent/ButtonComponent.js
+++ front/src/ButtonComponent/ButtonComponent.js
@@ -2,10 +2,11 @@ import React, { Component, useState } from 'react';
 import styles from './ButtonComponent.module.scss';
 import Button from '@material-ui/core/Button';
 import api from '../api/api';
-
+import translations from '../texts/texts.json';
+// Class for buttoncomponent for dayview.js to see states of loan devices
 class ButtonComponent extends Component {
     state = {
-        devices: [],
+        items: [],
     };
 
     componentDidMount() {
@@ -13,9 +14,10 @@ class ButtonComponent extends Component {
     }
 
     fetchDevices = async () => {
+        // Fetching device data
         try {
             const response = await api.getAllDevices();
-            this.setState({ devices: response });
+            this.setState({ items: response });
         } catch (error) {
             console.error('Error fetching devices:', error);
         }
@@ -23,7 +25,7 @@ class ButtonComponent extends Component {
 
     handleStatusChange = async (device) => {
         // Copy the current devices state
-        const devicesUpdated = this.state.devices.map(item => {
+        const devicesUpdated = this.state.items.map(item => {
             if (item.id === device.id) {
                 return { ...item, status: item.status === 'free' ? 'reserved' : 'free' };
             }
@@ -33,7 +35,7 @@ class ButtonComponent extends Component {
         try {
             // Returns the updated device
             await api.patchDevice(device.id, { status: device.status === 'free' ? 'reserved' : 'free' });
-            this.setState({ devices: devicesUpdated });
+            this.setState({ items: devicesUpdated });
         } catch (error) {
             console.error('Error updating device status:', error);
         }
@@ -42,7 +44,7 @@ class ButtonComponent extends Component {
     toggleText = (id) => {
         // On click changes text on button showing if 'free' or 'reserved'
         this.setState(prevState => ({
-            devices: prevState.devices.map(device => 
+            items: prevState.items.map(device => 
                 device.id === id ? { ...device, toggleText: !device.toggleText } : device
             ),
         }));
@@ -50,18 +52,20 @@ class ButtonComponent extends Component {
 
     render() {
         //Updates loan devices on a grid in dayview.js
-        const { devices } = this.state;
+        const lang = localStorage.getItem('language');
+        const { devices } = translations;
+        const { items } = this.state;
 
-        if (devices.length > 0) {
+        if (items.length > 0) {
             return (
                 <div className={styles.singularGrid}>
-                    {devices.map((device) => (
+                    {items.map((device) => (
                         <div key={device.id}>
                             <Button 
                             className={`${device.status === 'reserved' ? styles.reservedButton : styles.freeButton}`}
                             onClick={() => this.toggleText(device.id)}
                             >
-                                {device.toggleText ? (device.status === 'reserved' ? 'Reserved' : 'Free') : device.device_name}
+                                {device.toggleText ? (device.status === 'reserved' ? devices.Updated.ReservedStatus[lang] : devices.FreeStatus[lang]) : device.device_name}
                             </Button>
                         </div>
                     ))}
