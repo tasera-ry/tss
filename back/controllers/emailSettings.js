a4b91d6ebeb5ed4e63567be7e378ba818b46a816
Kalle Nyman <kalle.nyman@tuni.fi>
2024-10-18 16:02:30 +0300

    add verifyEmailCredentials to updateSettings in back-end

--- back/controllers/emailSettings.js
+++ back/controllers/emailSettings.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const root = path.join(__dirname, '..');
 const services = require(path.join(root, 'services'));
-const { sendPending, scheduleEmails } = require('../mailer.js');
+const { sendPending, scheduleEmails, verifyEmailCredentials } = require('../mailer.js');
 
 const controller = {
   read: async function readSettings(request, response) {
@@ -11,9 +11,18 @@ const controller = {
     return response.status(200).send(settings);
   },
   update: async function updateSettings(request, response) {
-    await services.emailSettings.update(request.body);
-    scheduleEmails(new Date(request.body.sendPendingTime));
-    return response.status(200).send();
+    // callback for verifyEmailCredentials
+    const updateEmailSettings = async (error, success) => {
+      console.log("updateEmailSettings error:", error, "success:", success)
+      if( error ) {
+        return response.status(400).send("Wrong credentials")
+      } else if (success) { 
+        await services.emailSettings.update(request.body);
+        scheduleEmails(new Date(request.body.sendPendingTime));
+        return response.status(200).send();
+      }
+    }
+    verifyEmailCredentials(request.body, updateEmailSettings);
   },
   sendPendingEmails: async function sendPendingEmails(request, response) {
     await sendPending();
