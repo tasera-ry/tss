b5cc1061745f5b8240e15685cec77b45f3d11e28
Lotta Lindell <61841036+basiliski@users.noreply.github.com>
2024-10-24 18:16:36 +0300

    Fix front unit tests (#245)
    * fix Utils.js unit tests
    
    * fix Dayview unit tests by mocking InfoBox
    
    * fix unit tests in monthview and weekview
    
    * fix mocks in Scheduling.test.js
    
    * add mock for InfoBox to fix unit tests for App.js
    
    * fix userManagement unit tests
    
    * add api mocks for rangeofficer tests
    
    * Update @testing-library/jest-dom
    
    * downgrade material-table version to evade known bugs

--- front/src/dayview/Dayview.test.js
+++ front/src/dayview/Dayview.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import Dayview from './Dayview';
 import api from '../api/api';
 import testUtils from '../_TestUtils/TestUtils';
 import * as axios from 'axios';
+
+// Mock the InfoBox component
+jest.mock('../infoBox/InfoBox', () => () => <div data-testid="mockInfoBox">Mock InfoBox</div>);
 
 jest.mock('axios');
 axios.get.mockResolvedValue({
@@ -82,6 +85,21 @@ describe('testing Dayview component', () => {
     );
     await waitFor(() =>
       expect(screen.getByText('Range closed')).toBeInTheDocument(),
+    );
+  });
+
+  it('should render the mocked InfoBox component', async () => {
+    api.getSchedulingDate = jest.fn(() => schedule);
+    localStorage.setItem('language', '1');
+    Date.now = jest.fn(() => '2020-10-21T11:30:57.000Z');
+
+    render(
+      <Router>
+        <Dayview history={history} match={{ params: { date } }} state={state} />
+      </Router>,
+    );
+    await waitFor(() =>
+      expect(screen.getByTestId('mockInfoBox')).toBeInTheDocument(),
     );
   });
 });
