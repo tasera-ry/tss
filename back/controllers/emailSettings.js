728849ae076193f42d028c91ab6572fff1a2ac4f
Kalle Nyman <kalle.nyman@tuni.fi>
2024-10-25 10:15:15 +0300

    return pass

--- back/controllers/emailSettings.js
+++ back/controllers/emailSettings.js
@@ -7,7 +7,7 @@ const controller = {
   read: async function readSettings(request, response) {
     const settings = await services.emailSettings.read();
     // The following line prevents the password from being sent out
-    settings.pass = '';
+    // settings.pass = '';
     return response.status(200).send(settings);
   },
   update: async function updateSettings(request, response) {
