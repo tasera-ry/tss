37c189253bc0fbc4990032140d60a8b87436657f
3zk1m0 <sander.kallio@gmail.com>
2025-03-06 22:48:37 +0200

    Rework device listing

--- front/src/Devices/devices.tsx
+++ front/src/Devices/devices.tsx
@@ -1,246 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
-import Container from '@mui/material/Container';
+import { useMemo } from 'react';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
   Button,
-  TextField,
-  IconButton,
-  Switch,
-  Tooltip,
 } from '@mui/material';
-import { Add, Edit, Delete, Save, Cancel } from '@mui/icons-material';
-import Styles from './devices.module.scss';
 import api from '../api/api';
-import { useCookies } from 'react-cookie';
-import { t } from '@lingui/core/macro';
+import colors from '../colors.module.scss';
+import { msg } from '@lingui/core/macro';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useLingui } from '@lingui/react/macro';
 
-const Devices = () => {
-  const [devices, setDevices] = useState([]);
-  const [editingRow, setEditingRow] = useState(null);
-  const [newRow, setNewRow] = useState({ device_name: '', status: 'free' });
-  const [cookies] = useCookies(['role']);
-  const userRole = cookies.role;
+export function DeviceStatusPanel() {
+  const { t } = useLingui();
 
-  const sortDevices = (devices) => {
-    // Function for sorting devices alphabetically
-    return devices.sort((a, b) => a.device_name.localeCompare(b.device_name));
-  };
+  const devicesQuery = useQuery({
+    queryKey: ['devices'],
+    queryFn: () => api.getAllDevices(),
+    select: (data) => {
+      return data.sort((a, b) => a.device_name.localeCompare(b.device_name))
+    },
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.getAllDevices();
-        const sortedDevices = sortDevices(response);
-        setDevices(sortedDevices);
-      } catch (error) {
-        console.error('Error fetching devices:', error);
-      }
-    };
+  if (devicesQuery.isLoading) {
+    return (
+      <div className='flex flex-col items-center justify-center py-4'>
+        <h2>{t`Loading devices...`}</h2>
+      </div>
+    )
+  }
 
-    fetchData();
-  }, []);
+  if (devicesQuery.isError) {
+    return (
+      <div className='flex flex-col items-center justify-center py-4'>
+        <h2>{t`Error loading devices`}</h2>
+      </div>
+    )
+  }
 
-  const handleStatusChange = async (rowData) => {
-    const data = [...devices];
-    const index = data.findIndex((item) => item.id === rowData.id);
-    data[index].status = rowData.status === 'free' ? 'reserved' : 'free';
-
-    await api.patchDevice(rowData.id, data[index]);
-    const sortedData = sortDevices(data);
-    setDevices(sortedData);
-  };
-
-  const handleAddRow = async () => {
-    if (!newRow.device_name.trim()) {
-      alert(t`Error: Device name cannot be empty`);
-      return;
-    }
-    newRow.status = translateStatus(newRow.status);
-    if (!newRow.status) {
-      alert(t`Error: Device status must be either 'free' or 'reserved'`);
-      return;
-    }
-    try {
-      const response = await api.createDevice(newRow);
-      const newDevice = response[0];
-      const updatedDevices = sortDevices([...devices, newDevice]);
-      setDevices(updatedDevices);
-      setNewRow({ device_name: '', status: 'free' });
-    } catch (error) {
-      console.error('Error adding device:', error);
-    }
-  };
-
-  const handleEditRow = async (index) => {
-    const deviceInfo = devices[index];
-    try {
-      await api.patchDevice(deviceInfo.id, editingRow);
-      const updatedData = devices.map((device, i) =>
-        i === index ? { ...device, ...editingRow } : device
-      );
-      setDevices(sortDevices(updatedData));
-      setEditingRow(null);
-    } catch (error) {
-      console.error('Error updating device:', error);
-    }
-  };
-
-  const handleDeleteRow = async (index) => {
-    const deviceInfo = devices[index];
-    try {
-      await api.deleteDevice(deviceInfo.id);
-      setDevices(devices.filter((_, i) => i !== index));
-    } catch (error) {
-      console.error('Error deleting device:', error);
-    }
-  };
-
-  const translateStatus = (status) => {
-    if (!status || status.trim() === '') {
-      return null;
-    }
-    const statusMap = {};
-    translations.devicesList.FreeStatus.forEach((s) => (statusMap[s.toLowerCase()] = 'free'));
-    translations.devicesList.ReservedStatus.forEach((s) => (statusMap[s.toLowerCase()] = 'reserved'));
-    return statusMap[status.toLowerCase()] || null;
-  };
+  if (!devicesQuery.data || devicesQuery.data.length === 0) {
+    return (
+      <div className='flex flex-col items-center justify-center py-4'>
+        <h2>{t`No devices found`}</h2>
+      </div>
+    )
+  }
 
   return (
-    <div>
-      {devices ? (
-        <ScopedCssBaseline>
-          <Container style={{ maxWidth: '900px', padding: '10px' }}>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>{t`Name`}</TableCell>
-                    <TableCell>{t`Status (free/reserved)`}</TableCell>
-                    <TableCell>`{t`Actions`}</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {devices.map((row, index) => (
-                    <TableRow key={row.id}>
-                      {editingRow && editingRow.index === index ? (
-                        <>
-                          <TableCell>
-                            <TextField
-                              value={editingRow.device_name}
-                              onChange={(e) =>
-                                setEditingRow({ ...editingRow, device_name: e.target.value })
-                              }
-                            />
-                          </TableCell>
-                          <TableCell>
-                            <Switch
-                              checked={editingRow.status === 'reserved'}
-                              onChange={() =>
-                                setEditingRow({
-                                  ...editingRow,
-                                  status: editingRow.status === 'free' ? 'reserved' : 'free',
-                                })
-                              }
-                              name="status-switch"
-                              inputProps={{ 'aria-label': 'status-switch' }}
-                              classes={{
-                                root: editingRow.status === 'free' ? Styles.freeSwitch : Styles.reservedSwitch,
-                                track: Styles.track,
-                                thumb: Styles.thumb,
-                              }}
-                            />
-                          </TableCell>
-                          <TableCell>
-                            <IconButton onClick={() => handleEditRow(index)}>
-                              <Save />
-                            </IconButton>
-                            <IconButton onClick={() => setEditingRow(null)}>
-                              <Cancel />
-                            </IconButton>
-                          </TableCell>
-                        </>
-                      ) : (
-                        <>
-                          <TableCell>{row.device_name}</TableCell>
-                          <TableCell>
-                            <Switch
-                              checked={row.status === 'reserved'}
-                              onChange={() => handleStatusChange(row)}
-                              name="status-switch"
-                              inputProps={{ 'aria-label': 'status-switch' }}
-                              classes={{
-                                root: row.status === 'free' ? Styles.freeSwitch : Styles.reservedSwitch,
-                                track: Styles.track,
-                                thumb: Styles.thumb,
-                              }}
-                            />
-                          </TableCell>
-                          <TableCell>
-                            {userRole === 'superuser' && (
-                              <>
-                                <Tooltip title={t`Edit`}>
-                                  <IconButton onClick={() => setEditingRow({ ...row, index })}>
-                                    <Edit />
-                                  </IconButton>
-                                </Tooltip>
-                                <Tooltip title={t`Delete`}>
-                                  <IconButton onClick={() => handleDeleteRow(index)}>
-                                    <Delete />
-                                  </IconButton>
-                                </Tooltip>
-                              </>
-                            )}
-                          </TableCell>
-                        </>
-                      )}
-                    </TableRow>
-                  ))}
-                  {userRole === 'superuser' && (
-                    <TableRow>
-                      <TableCell>
-                        <TextField
-                          value={newRow.device_name}
-                          onChange={(e) => setNewRow({ ...newRow, device_name: e.target.value })}
-                          placeholder={t`Name`}
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <Switch
-                          checked={newRow.status === 'reserved'}
-                          onChange={() =>
-                            setNewRow({ ...newRow, status: newRow.status === 'free' ? 'reserved' : 'free' })
-                          }
-                          name="status-switch"
-                          inputProps={{ 'aria-label': 'status-switch' }}
-                          classes={{
-                            root: newRow.status === 'free' ? Styles.freeSwitch : Styles.reservedSwitch,
-                            track: Styles.track,
-                            thumb: Styles.thumb,
-                          }}
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <Button onClick={handleAddRow} startIcon={<Add />}>
-                          {t`Add`} 
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Container>
-        </ScopedCssBaseline>
-      ) : null}
+    <div className='flex flex-col items-center justify-center py-4'>
+      <h2 className='text-2xl font-bold'>{t`Devices`}</h2>
+      <div className='flex flex-row justify-center flex-wrap gap-2 p-4'>
+        {devicesQuery.data.map((device) => (
+          <DeviceCard key={device.id} device={device} />
+        ))}
+      </div>
     </div>
-  );
+  )
+
 };
 
-export default Devices;
+const deviceStatuses = {
+  free: {
+    statusMsg: msg`Free`,
+    color: colors.green,
+  },
+  reserved: {
+    statusMsg: msg`Reserved`,
+    color: colors.redLight,
+  }
+}
+
+
+function DeviceCard({ device }) {
+  const { i18n } = useLingui();
+  const queryClient = useQueryClient()
+
+  const buttonState = useMemo(() => deviceStatuses[device.status], [device.status])
+
+  const deviceStatusMutation = useMutation({
+    mutationFn: () => api.patchDevice(device.id, { status: device.status === 'free' ? 'reserved' : 'free' }),
+    onSuccess: () => {
+      queryClient.setQueryData(['devices'], (old: any) => {
+        return old.map((d) => d.id === device.id ? { ...d, status: device.status === 'free' ? 'reserved' : 'free' } : d)
+      })
+    }
+  })
+
+
+  return (
+    <Button
+      style={{ backgroundColor: buttonState.color }}
+      size="large"
+      variant="contained"
+      className='w-[230px] flex flex-col items-center justify-center hover:before:content-[""] before:absolute before:inset-0 hover:before:bg-black/20 before:z-10 before:rounded'
+      onClick={() => deviceStatusMutation.mutate()}
+      disabled={deviceStatusMutation.isLoading}
+      data-testid={`device-card-${device.id}`}
+    >
+      <span>{device.device_name}</span>
+      <span className='text-sm'>{i18n._(buttonState.statusMsg)}</span>
+    </Button>
+  )
+}
