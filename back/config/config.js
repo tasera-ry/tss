f30264f6113ba479238f6290d3fb90d8b4838546
Roope Kärkkäinen <roopeak@protonmail.com>
2025-04-04 09:51:33 +0300

    Change startDate to cover current date minus one year

--- back/config/config.js
+++ back/config/config.js
@@ -21,7 +21,7 @@ const config = {
     users: 10,
     ranges: 1,
     tracks: 7,
-    startDate: new Date().toISOString().slice(0,10),
+    startDate: new Date(Date.now() - 365 * 24*60*60*1000).toISOString().slice(0,10),
     endDate: new Date(Date.now() + 365 * 24*60*60*1000).toISOString().slice(0,10),
     // chunkSize = how many rows are inserted in a single insertion, larger
     // chunk size equals faster insertions, but a value that is too high causes
