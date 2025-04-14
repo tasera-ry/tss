aaff16907ee3cf1e1e7e5341dada8ef55e98fc69
3zk1m0 <sander.kallio@gmail.com>
2025-04-14 19:15:50 +0300

    Fix wrong import

--- front/src/pages/UserManagement/components/SelectAssociation.tsx
+++ front/src/pages/UserManagement/components/SelectAssociation.tsx
@@ -1,17 +1,16 @@
-import { Select, useLingui } from '@lingui/react/macro';
-import { FormControl, InputLabel, MenuItem } from '@mui/material';
+import { useLingui } from '@lingui/react/macro';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 export function SelectAssociation({ form, associationList }) {
   const { t } = useLingui();
-
   if (form.watch('role') !== 'rangeofficer') return null;
 
   return (
     <FormControl>
       <InputLabel id="association-select-label">{t`Select association`}</InputLabel>
       <Select
-        labelId="association-select-label"
         id="associationSelect"
+        labelId="association-select-label"
         label={t`Select association`}
         value={form.watch('associationId') || ''}
         onChange={(e) => form.setValue('associationId', e.target.value)}
